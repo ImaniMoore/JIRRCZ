4d1190efda3c63b0859575677a40419577cb19b5
Imani Moore <imanimoore413@gmail.com>
2025-11-10 18:05:45 -0500

    fix

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,3 +1,27 @@
-var submit_button=document.getElementById('Submit')
+function validateForm(e) {
+  event.preventDefault();
+  let x = document.forms["myForm"]["Fname"].value;
+  let y = document.forms["myForm"]["Lname"].value;
+  let z = document.forms["myForm"]["email"].value;
+  let a = document.forms["myForm"]["phone"].value;
+  if (x == "") {
+    alert("First Name must be filled out");
+    return false;
+  } else if (y == "") {
+    alert("Last Name must be filled out");
+    return false;
+  } else if (z == "") {
+    alert("Email must be entered");
+    return false;
+  } else if (a == "") {
+    alert("Phone number must be entered");
+    return false;
+  }
 
-submit_button addEventListener(oncliick,submit_button)
+  alert("Submitted");
+  return true;
+}
+
+let contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", validateForm);
